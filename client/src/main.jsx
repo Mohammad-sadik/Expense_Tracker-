a8ae8684d763326098556975db67f01f15c3d67f
chatura Sri <chaturasri2001.27@gmail.com>
2026-02-15 05:32:04 +0530

    Configure frontend for production deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import axios from 'axios'
+
+// Set base URL for production depending on environment variable
+if (import.meta.env.VITE_API_URL) {
+    axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
